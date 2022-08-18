cbc395a6d544bc90d8e6074756705ec27605fd48
Jeffrey W <jhwhiting@gmail.com>
2022-08-18 08:51:44 -0400

    First commit

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace scratchpackage;
+
+public class Class1
+{
+    public void ViewStack()
+    {
+        Console.WriteLine("Hello, World!");
+
+        var stack = new Stack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        foreach (var i in stack)
+        {
+            Console.WriteLine(i);
+        }
+
+        if (stack.TryPeek(out var n))
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
